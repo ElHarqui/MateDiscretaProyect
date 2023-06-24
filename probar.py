dff7e8b679098a525896bfff71b82e248d0fa845
Sebastian <sebastian.peralta@unmsm.edu.pe>
2023-06-24 08:11:04 -0500

    Initial commit

---
+++ probar.py
@@ -0,0 +1,15 @@
+class restaurante( ): 
+  def __init__(self,name = 'HOLS',cocina = 'ADIO'):
+    self.restaurant_name =  name
+    self.cuisine_name = cocina
+  def describe_restaurant(self):
+    return f"El nombre del restaurante es {self.restaurant_name} y su tipo de cocina es {self.cuisine_name}"
+  #def open_restaurant(self):
+    
+try:
+  gaaa = restaurante()
+  print(gaaa.restaurant_name)
+  #print(gaaa)
+except Exception as a:
+  print(a)
+  print("ERROR")
---
+++ projecto1.py
@@ -0,0 +1,71 @@
+class Alumno :
+    def __init__(self, nombre, codigo):
+        self.nombre = nombre
+        self.codigo = codigo
+        
+Preg1 = {
+    "1"  :  "futbol",
+    "2" :  "basquet",
+    "3"  :  "voley",
+    "4" :  "natacion",
+    "5" :  "karate",
+    "6"  :  "otros"
+}
+Preg2 = {
+    "1" : "salsa",
+    "2" : "rock",
+    "3" : "bachata",
+    "4" : "regaetton",
+    "5" : "merengue",
+    "6" : "technocumbia",
+    "7" : "folklorica",
+    "8" : "otros"
+}
+Preg3 = Preg2
+Preg4 = {
+    "1" : "guitarra",
+    "2" : "bateria",
+    "3" : "piano",
+    "4" : "saxo",
+    "5" : "no toca",
+    "6" : "otros"
+}
+Preg5 = {
+    "1" : "x",
+    "2" : "y",
+    "3" : "z",
+    "4" : "d",
+    "5" : "p"
+}
+Preg6 = {
+    "1" : "cine",
+    "2" : "visitar museos",
+    "3" : "viajar",
+    "4" : "oratoria",
+    "5" : "videojuegos",
+    "6" : "conciertos",
+    "7" : "otros"
+}
+Preg = [Preg1,Preg2,Preg3,Preg4,Preg5,Preg6] 
+Persona = list()   
+
+menu = "\t\tMENU\t1.- PREGUNTAS"
+#x = Preg.keys()
+#print(Preg[1].keys())
+b = str()
+h = ["1.- Cual es su deporte favorito?","2.- Que musica le gusta ?","3.- Que danza le gusta de preferencia?","4.- Toca algun instrumento?","5.- Cual es su clib favorito?","6.- Cual es su hobbie favorito?"]
+print("Cuantas personas son ?")
+n = int(input("-> "))
+for k in range (n) :
+    print(f'Ingrese nombre del alumno N°{k+1} :')
+    nombre = input("-> ")
+    for i in range (6)  :
+        print(h[i])
+        r = list(Preg[i].keys())
+        for j in range(len(r)) :
+            print(f'{r[j]} :   {Preg[i][r[j]]}')
+        a = input('-> ')
+        b = b + a    
+    Persona.append(f'Persona{k}')
+    Persona[k] = Alumno(nombre , b)
+    #*GUARDAR b EN UNA ESTRUCTURA
